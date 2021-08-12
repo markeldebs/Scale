589ea94e293c846a7119696c4f7770608bde4e50
mark.eldebs <mark.eldebs@hotmail.ca>
2021-08-12 01:15:27 -0400

    Working Tap With Recurtion Control

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,29 @@
 int tvalue = 0;
 int buttonVal = 0;
 int buttonLast = 0;
+int n = 0;
+
+
+
 
 int checkButton (){
-    int event = 0;
-     buttonVal = analogRead(touchpin);
-         
-    if (buttonVal >= 3500 ){
-          Serial.println("pressed");
-          tvalue = tvalue++;
+    //int event = 0;
+    buttonVal = analogRead(touchpin);
+    if (buttonVal < 3500){
+        //Serial.println("Released");
+        tvalue = 0;
+        n = 0;
 
     }
-    if (buttonVal < 3500){
-        Serial.println("Released");
-        tvalue = 0;
+    else if (buttonVal >= 3500 && n == 0 ){
+          tvalue++;
+          if (tvalue >= 45 && tvalue <= 60){
+          Serial.println("Tap");
+          tvalue = 0;
+          n =1;
+        }
     }
-    delay (100);
+    //delay (100);
      return tvalue;
 }
 
